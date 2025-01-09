3ebab7953b9956ea767fd48eeb7c359f7a84f29e
“Lucas <lucasromao2006@icloud.com>
2025-01-09 13:44:45 -0300

    Botão de salvar cadastrando produto

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,18 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.sql.SQLException;
 
 
 public class ProdutosDAO {
@@ -22,12 +13,24 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
+    public int cadastrarProduto (ProdutosDTO produto){
+        int status;
+        try{
+            
+            conn = new conectaDAO().connectDB();
+            prep = conn.prepareStatement("INSERT INTO produtos (nome,valor,status) VALUES(?,?,?)");
+            prep.setString(1,produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            status = prep.executeUpdate();
+            return status;
+            
+        } catch(SQLException ex){
+            
+        System.out.println("Erro ao conectar: " + ex.getMessage() + " ->>" + ex.getErrorCode());
+        return ex.getErrorCode();
         
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
