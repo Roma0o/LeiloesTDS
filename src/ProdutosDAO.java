af02b40ca356e932af41b4ec7bf36c6c87e95ee5
“Lucas <lucasromao2006@icloud.com>
2025-01-14 15:29:19 -0300

    listagem de produtos funcionando

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -4,6 +4,7 @@ import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.sql.SQLException;
+import java.util.List;
 
 
 public class ProdutosDAO {
@@ -16,7 +17,7 @@ public class ProdutosDAO {
     public int cadastrarProduto (ProdutosDTO produto){
         int status;
         try{
-            
+            //listagem.add(produto);
             conn = new conectaDAO().connectDB();
             prep = conn.prepareStatement("INSERT INTO produtos (nome,valor,status) VALUES(?,?,?)");
             prep.setString(1,produto.getNome());
@@ -29,13 +30,18 @@ public class ProdutosDAO {
             
         System.out.println("Erro ao conectar: " + ex.getMessage() + " ->>" + ex.getErrorCode());
         return ex.getErrorCode();
-        
         }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
         
         return listagem;
+    }
+    
+
+    
+    public void venderProduto(int parseInt) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
     
     
