e9d0768d22e7e8f3587211678dc1c81911832e11
“Lucas <lucasromao2006@icloud.com>
2025-01-27 18:16:42 -0300

    Tela de vendas

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -33,27 +33,77 @@ public class ProdutosDAO {
         }
     }
     
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        return listagem;
-    }
-    
-
-    
-    public void venderProduto(int parseInt) {
+    public boolean venderProduto(int parseInt) {
+        boolean status;
         try{
-        conn = new conectaDAO().connectDB();
-        prep = conn.prepareStatement("UPDATE produtos SET status ? WHERE id = ?");
-        prep.setString(1, "Vendido");
-        prep.setInt(2, parseInt);
-        prep.executeUpdate();
-        
-        }catch(SQLException ex){
+            conn = new conectaDAO().connectDB();
+            prep = conn.prepareStatement("UPDATE produtos SET status = ? WHERE id = ?");
+            prep.setString(1, "Vendido");
+            prep.setInt(2, parseInt);
+            prep.executeUpdate();
+            status = true;
+            return status;
+            
+        } catch(SQLException ex){
             System.out.println("Erro ao atualizar BD: " + ex.getMessage() + " ->>" + ex.getErrorCode());
+            status = false;
+            return status;
         }
     }
     
+    public List<ProdutosDTO> getProdutos(){
+    String sql = "SELECT * FROM produtos";
     
+    try{
+        conn = new conectaDAO().connectDB();
+        PreparedStatement stmt = this.conn.prepareStatement(sql);
+        ResultSet rs = stmt.executeQuery();
+            
+        List<ProdutosDTO> listaProdutos = new ArrayList<>();
+            
+        while (rs.next()) { //.next retorna verdadeiro caso exista uma próxima posição dentro do array
+            ProdutosDTO produto = new ProdutosDTO();
+                        
+            produto.setId(rs.getInt("id"));
+            produto.setNome(rs.getString("nome"));
+            produto.setValor(rs.getInt("valor"));
+            produto.setStatus(rs.getString("status"));
+                        
+            listaProdutos.add(produto);    
+        }
+        return listaProdutos;
+            
+            
+    }catch(Exception e){
+        JOptionPane.showMessageDialog(null,"Erro ao preencher a tabela! " + e);
+        return null;
+    }
+    }
     
-        
+    public List<ProdutosDTO> listarProdutosVendidos(){
+        boolean status;
+        try{
+            conn = new conectaDAO().connectDB();
+            prep = conn.prepareStatement("SELECT * FROM produtos WHERE status = ?");
+            prep.setString(1, "Vendido");
+            ResultSet rs = prep.executeQuery();
+            List<ProdutosDTO> listaProdutos = new ArrayList<>();
+            while (rs.next()) { //.next retorna verdadeiro caso exista uma próxima posição dentro do array
+                ProdutosDTO produto = new ProdutosDTO();
+                        
+                produto.setId(rs.getInt("id"));
+                produto.setNome(rs.getString("nome"));
+                produto.setValor(rs.getInt("valor"));
+                produto.setStatus(rs.getString("status"));
+                        
+                listaProdutos.add(produto);    
+            }
+            return listaProdutos;
+        } catch(SQLException ex){
+            System.out.println("erro");
+            status = false;
+            return null;
+        }
+    }
 }
 
